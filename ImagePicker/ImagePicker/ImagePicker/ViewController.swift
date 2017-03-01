067a3d9af324108be27cbbc0149f7cd8ed63cae8
Appinventiv <mansi2mittal@gmail.com>
2017-03-01 15:10:35 +0530

    no message

--- ImagePicker/ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ImagePicker/ViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 
 class ViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
     
+    var arrayOfUIImagePickerView = [" RED", " BLUE", " YELLOW"]
+    
+    var placeHolder = 0
+    
     // OUTLETS
     
     @IBOutlet weak var image: UIImageView!
     
     @IBOutlet weak var chooseImageButton: UIButton!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         
-        super.viewDidLoad()
+    super.viewDidLoad()
         
-        imagePickerController.delegate = self
+    // ASSIGNING THE DELEGATE OF THE IMAGE PICKER
+        
+    imagePickerController.delegate = self
+      
+    // ASSINGNING THE DATASOURCE AND DELEGATES OF THE PICKER VIEW
+        
+    pickerView.dataSource = self
+    pickerView.delegate = self
+        
        }
     
     let imagePickerController = UIImagePickerController()
@@ -30,8 +46,6 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
     @IBAction func chooseImageButtonTapped(_ sender: Any) {
         
         let imagePickerController = UIImagePickerController()
-        
-        imagePickerController.delegate = self
         
         let actionSheet = UIAlertController(title: "Photo source", message: "Choose Photo", preferredStyle: .actionSheet)
         
@@ -91,6 +105,42 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         
         self.dismiss(animated: true, completion: nil )
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+         label.text = arrayOfUIImagePickerView[row]
+    }
+    
+    // ON THE TAP OF THE SUBMIT BUTTON THE TEXT OF THE LABEL WOULD CHANGE
+    
+}
+
+ // MARK: EXTENSION OF UIPICKER VIEW
+
+extension ViewController : UIPickerViewDelegate , UIPickerViewDataSource
+    
+{
+    // RETURNING THE COMPONENTS IN A PICKER VIEW
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    // RETURNING THE NUMBER OF ROWS IN COMPONENT
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return arrayOfUIImagePickerView.count
+    }
+    
+    // RETURNING THE TITLE FOR ROW
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+       
+        return arrayOfUIImagePickerView[row]
     }
     
 }
